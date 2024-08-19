9ee6b4adfd4014559e87a173ef6e2aa011c1f6d3
2scent <ad7549@gmail.com>
2024-08-19 20:57:59 +0900

    chore: shared vite 기반으로 재구성

--- packages/shared/date.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatDate = formatDate;
-function formatDate(date) {
-    return date.toISOString().split('T')[0];
-}
---
+++ packages/shared/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'SharedLib',
+      fileName: (format) => `shared.${format}.js`,
+    },
+  },
+});
