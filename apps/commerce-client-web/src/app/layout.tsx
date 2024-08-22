49848b839836c7a19eca63e73f2f9cc67331ead0
yoo94 <ahy0119@naver.com>
2024-08-23 01:16:45 +0900

    feat: layout 초기 setting

--- apps/commerce-client-web/src/app/layout.tsx
+++ apps/commerce-client-web/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./layout/components/header";
+import Footer from "./layout/components/footer";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
