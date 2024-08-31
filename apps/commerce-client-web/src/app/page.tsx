1bc86d5c7ece1de600dda0c3482e669d007158ef
younggil <zelord.kwoun@gmail.com>
2024-08-31 09:58:03 +0900

    chore: develop/client rebase

--- apps/commerce-client-web/src/app/page.tsx
+++ apps/commerce-client-web/src/app/page.tsx
@@ -1,9 +1,8 @@
-import SliderBanner from "@/app/components/slidebanner";
-
+import SliderBanner from '@/app/components/slidebanner';
 
 export const mockImages = [
-    { src: '/mockbannerimage/book1.png', alt: 'Banner 1' },
-    { src: '/mockbannerimage/book2.png', alt: 'Banner 2' },
+  { src: '/mockbannerimage/book1.png', alt: 'Banner 1' },
+  { src: '/mockbannerimage/book2.png', alt: 'Banner 2' },
 ];
 
 export default function Home() {
