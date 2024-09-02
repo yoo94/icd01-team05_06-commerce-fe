1bb3212a3c712f6e230d5e610e0888f5350de72e
2scent <ad7549@gmail.com>
2024-09-02 20:57:40 +0900

    feat: orders 페이지 mock data 사용

--- apps/commerce-client-web/src/app/my-page/components/order-table.tsx
+++ apps/commerce-client-web/src/app/my-page/components/order-table.tsx
@@ -6,18 +6,16 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import orders from '@/data/orders.json';
+import { format } from 'date-fns';
 
-const orders = [
-  {
-    orderId: 131231313,
-    orderDate: new Date(),
-    title: '딸아, 돈 공부 절대 미루지 마라',
-    price: 16200,
-    amount: 1,
-    orderer: '이향기',
-    recipient: '이향기',
-  },
-];
+function formatDate(date: string) {
+  return format(date, 'yyyy.MM.dd');
+}
+
+function formatTitle(items: { book: { title: string } }[]) {
+  return items.map((item) => item.book.title).join(', ');
+}
 
 export default function OrderTable() {
   return (
@@ -26,27 +24,23 @@ export default function OrderTable() {
         <TableRow>
           <TableHead>주문번호</TableHead>
           <TableHead>주문일자</TableHead>
-          <TableHead>주문내역</TableHead>
-          <TableHead>주문금액/수량</TableHead>
+          <TableHead className="w-[400px]">주문내역</TableHead>
+          <TableHead>주문금액</TableHead>
           <TableHead>주문상태</TableHead>
           <TableHead>배송조회</TableHead>
           <TableHead>주문자</TableHead>
-          <TableHead>수령자</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {orders.map((order) => (
-          <TableRow key={order.orderId}>
-            <TableCell>{order.orderId}</TableCell>
-            <TableCell>{order.orderDate.toLocaleDateString()}</TableCell>
-            <TableCell>{order.title}</TableCell>
-            <TableCell>
-              {order.price}/{order.amount}
-            </TableCell>
-            <TableCell>배송완료</TableCell>
+          <TableRow key={order.id}>
+            <TableCell>{order.id}</TableCell>
+            <TableCell>{formatDate(order.orderDate)}</TableCell>
+            <TableCell>{formatTitle(order.orderItems)}</TableCell>
+            <TableCell>{order.finalPrice}</TableCell>
+            <TableCell>{order.status}</TableCell>
             <TableCell>조회</TableCell>
-            <TableCell>{order.orderer}</TableCell>
-            <TableCell>{order.recipient}</TableCell>
+            <TableCell>{order.user.username}</TableCell>
           </TableRow>
         ))}
       </TableBody>
