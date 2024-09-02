46cc8f6d1cc92091b6b52277e110d369837007c1
yoo94 <ahy0119@naver.com>
2024-09-02 21:33:38 +0900

    feat: 카트추가 함수 연결

--- apps/commerce-client-web/src/app/components/(searchResult)/SearchResult.tsx
+++ apps/commerce-client-web/src/app/components/(searchResult)/SearchResult.tsx
@@ -1,19 +1,21 @@
 // SearchResult.tsx
 import React from 'react';
 import ProductCard from '@/app/components/(searchResult)/productCard';
-import { SearchResultProps } from '@/types/productTypes'; // 인터페이스를 가져오기
+import { Product, SearchResultProps } from '@/types/productTypes';
+import useCartStore from '@/stores/useCartStore'; // 인터페이스를 가져오기
 
-function SearchResult({ products, onAddToCart, onBuyNow }: SearchResultProps) {
+function SearchResult({ products, onBuyNow }: SearchResultProps) {
+  const { addProduct } = useCartStore();
   return (
     <div className="space-y-4">
-      {products.map((product) => (
+      {products.map((product: Product) => (
         <ProductCard
           key={product.id}
           id={product.id}
           imageUrl={product.imageUrl}
           title={product.title}
           price={product.price}
-          onAddToCart={() => onAddToCart(product.id)}
+          onAddToCart={() => addProduct(product)}
           onBuyNow={() => onBuyNow(product.id)}
         />
       ))}
