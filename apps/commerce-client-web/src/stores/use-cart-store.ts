4bc80b5557955a3eb80827194d659c0350a22453
yoo94 <ahy0119@naver.com>
2024-09-07 16:40:39 +0900

    reafctor : deprecated 수정

--- apps/commerce-client-web/src/stores/use-cart-store.ts
+++ apps/commerce-client-web/src/stores/use-cart-store.ts
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create }  from 'zustand';
 import { persist } from 'zustand/middleware';
 import { Product } from '@/types/productTypes';
 import { CartItem } from '@/types/carttypes';
