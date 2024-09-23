cac0aaad50ef581172ecfe4517d0fc9cdf511309
DahnBee Shim <colleksql@8snippet.com>
2024-09-23 21:09:22 +0900

    feat: side bar router 설정

--- apps/commerce-client-web/src/app/(default)/my-page/components/side-bar.tsx
+++ apps/commerce-client-web/src/app/(default)/my-page/components/side-bar.tsx
@@ -1,21 +1,45 @@
+import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import MenuList from './menu-list';
 
-const orderListItems = ['주문내역/배송조회'];
-const userInfoItems = ['배송주소록', '회원정보관리', '마케팅 수신 및 정보 제공 동의 관리'];
+const menuItems = [
+  {
+    title: '주문내역',
+    items: [{ title: '주문내역/배송조회', route: '/orders' }],
+  },
+  {
+    title: '회원정보',
+    items: [
+      { title: '배송주소록', route: '/shipping-address' },
+      { title: '회원정보관리', route: '/user-info' },
+      { title: '마케팅 수신 및 정보 제공 동의 관리', route: '/marketing-consent' },
+    ],
+  },
+];
+
+const sideBarButtons = [
+  { title: '찜 목록', route: '/wishlist' },
+  { title: '나의 1:1 문의 내역', route: '/inquiries' },
+  { title: '나의 리뷰', route: '/reviews' },
+];
 
 const SideBar = () => {
   return (
     <aside className="font-nanumeneo flex min-h-[800px] w-[200px] flex-col gap-6 rounded-md border p-4 text-sm">
-      <MenuList title="주문내역" items={orderListItems} />
-      <Separator />
-      <MenuList title="회원정보" items={userInfoItems} />
-      <Separator />
+      {menuItems.map((menu, index) => (
+        <div key={index}>
+          {/* MenuList 컴포넌트에 title과 items (route 정보 포함) 전달 */}
+          <MenuList title={menu.title} items={menu.items} />
+          <Separator />
+        </div>
+      ))}
       <div className="mt-8 flex flex-col gap-4">
-        <Button>찜 목록</Button>
-        <Button>나의 1:1 문의 내역</Button>
-        <Button>나의 리뷰</Button>
+        {sideBarButtons.map((button, index) => (
+          <Button key={index} asChild>
+            <Link href={button.route}>{button.title}</Link>
+          </Button>
+        ))}
       </div>
     </aside>
   );
--- apps/commerce-client-web/src/app/(default)/my-page/layout.tsx
+++ apps/commerce-client-web/src/app/(default)/my-page/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex gap-12">
+    <div className=" flex gap-12">
       <SideBar />
       <div className="flex-1">{children}</div>
     </div>
