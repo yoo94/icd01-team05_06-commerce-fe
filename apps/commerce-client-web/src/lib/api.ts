5c0b70f220932fd03dc438c9e5bbfe867f2a2cb1
2scent <ad7549@gmail.com>
2024-09-24 21:16:20 +0900

    fix: beforeRequest 주석 처리

--- apps/commerce-client-web/src/lib/api.ts
+++ apps/commerce-client-web/src/lib/api.ts
@@ -8,15 +8,18 @@ export const baseApi = ky.create({
     statusCodes: [401],
   },
   hooks: {
-    beforeRequest: [
-      async (request) => {
-        const session = await getSession();
-
-        if (session?.accessToken) {
-          request.headers.set('Authorization', `Bearer ${session.accessToken}`);
-        }
-      },
-    ],
+    // getSession()을 호출하면 NEXTAUTH_URL/api/auth/session으로 요청을 보냄
+    // 빌드 단계에서는 이 요청이 무조건 실패하기 때문에 에러 발생
+    // 토큰을 넣어주는 다른 방식을 생각해봐야 할 듯
+    // beforeRequest: [
+    //   async (request) => {
+    //     const session = await getSession();
+    //
+    //     if (session?.accessToken) {
+    //       request.headers.set('Authorization', `Bearer ${session.accessToken}`);
+    //     }
+    //   },
+    // ],
     beforeRetry: [
       async ({ request, error, retryCount }) => {
         const responseError = error as HTTPError; // Cast error to ky.HTTPError to access response
