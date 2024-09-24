02add473fe7e92a96f48b727863c235d3e953897
yoo94 <ahy0119@naver.com>
2024-09-25 00:49:22 +0900

    -feat: vitest example

--- apps/commerce-client-web/vitest.config.ts
+++ apps/commerce-client-web/vitest.config.ts
@@ -6,10 +6,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // src 폴더를 절대 경로로 설정
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
     environment: 'jsdom',
+    setupFiles: ['./setupTests.ts'],
   },
 });
