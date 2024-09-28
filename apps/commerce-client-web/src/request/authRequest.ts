97c70542d91f58e4209067dfb7dd7c4d787cf62d
2scent <ad7549@gmail.com>
2024-09-28 12:09:55 +0900

    feat: request 모듈 추가

---
+++ apps/commerce-client-web/src/request/authRequest.ts
@@ -0,0 +1,5 @@
+// import { baseApi } from '@/lib/api';
+//
+// const authApi = baseApi.extend({
+//   prefixUrl: process.env.NEXT_PUBLIC_AUTH_API_URL,
+// });
---
+++ apps/commerce-client-web/src/request/productRequest.ts
@@ -0,0 +1,5 @@
+// import { baseApi } from '@/lib/api';
+//
+// const productApi = baseApi.extend({
+//   prefixUrl: process.env.NEXT_PUBLIC_PRODUCT_API_URL,
+// });
