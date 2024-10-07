210c519f38607c0fe19454109ce122da613b3de0
DahnBee Shim <colleksql@8snippet.com>
2024-10-08 08:12:49 +0900

    style: 반응형 작업 진행

--- apps/commerce-client-web/src/app/(default)/my-page/layout.tsx
+++ apps/commerce-client-web/src/app/(default)/my-page/layout.tsx
@@ -7,9 +7,14 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className=" flex gap-10">
-      <SideBar />
-      <div className="flex-1">{children}</div>
+    <div className="flex flex-col md:flex-row md:gap-10">
+      {/* Sidebar, visible on medium screens and larger */}
+      <div className="hidden md:block md:w-1/4">
+        <SideBar />
+      </div>
+
+      {/* Main content area */}
+      <div className="flex-1 p-4">{children}</div>
     </div>
   );
 };
--- apps/commerce-client-web/src/app/(default)/my-page/user-info/page.tsx
+++ apps/commerce-client-web/src/app/(default)/my-page/user-info/page.tsx
@@ -1,13 +1,21 @@
+import { getUserInfo } from '@/app/actions/auth-action';
 import UserInfoForm from './components/user-info-form';
 
 const Page = async () => {
-  return (
-    <div className="mx-auto">
-      <h1 className="mb-4">회원정보 수정</h1>
-      {/* 클라이언트 컴포넌트인 Form */}
-      <UserInfoForm />
-    </div>
-  );
+  try {
+    const userInfo = await getUserInfo(); // Fetch user data on the server side
+
+    return (
+      <div className="mx-auto">
+        <h1 className="mb-4">회원정보 수정</h1>
+        {/* Pass the fetched user info as a prop to the client-side form */}
+        <UserInfoForm userInfo={userInfo} />
+      </div>
+    );
+  } catch (error) {
+    console.error('Error during fetching user info:', error);
+    return <div>회원정보를 불러오는 중 오류가 발생했습니다.</div>;
+  }
 };
 
 export default Page;
