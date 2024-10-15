cf993801029247d0b9c6e8c4523f3c75ef298b06
DahnBee Shim <colleksql@8snippet.com>
2024-10-15 19:36:13 +0900

    chore: order-action conflict 해결

--- apps/commerce-client-web/src/app/actions/order-action.ts
+++ apps/commerce-client-web/src/app/actions/order-action.ts
@@ -2,16 +2,38 @@
 
 import type { SearchParamsOption } from 'ky';
 import type { ApiResponse } from '@/types/api-types';
-import type {
-  DateRange,
-  DetailOrder,
-  OrdersResponse,
-  OrderStatus,
-  SortBy,
-} from '@/types/order-types';
-import { externalApi } from '@/lib/api';
-import { getHeadersWithToken } from './utils/action-helper';
+import type { DateRange, OrdersResponse, OrderStatus, SortBy } from '@/types/order-types';
+import { externalApi, orderApi, productApi } from '@/lib/api';
 import { Product } from '@/types/product-types';
+import { getHeadersWithToken } from './utils/action-helper';
+interface CreateOrderRequest {
+  products: {
+    id: number;
+    quantity: number;
+  }[];
+  ordererInfo: {
+    name: string;
+    phoneNumber: string;
+    email: string;
+  };
+  deliveryInfo: {
+    recipient: string;
+    phoneNumber: string;
+    streetAddress: string;
+    detailAddress: string;
+    postalCode: string;
+    memo?: string;
+  };
+  paymentInfo: {
+    method: string;
+    depositorName: string;
+  };
+  agreementInfo: {
+    termsOfService: boolean;
+    privacyPolicy: boolean;
+    ageVerification: boolean;
+  };
+}
 
 interface GetOrdersParams {
   dateRange?: DateRange;
@@ -84,24 +106,6 @@ export const getOrders = async (params: GetOrdersParams): Promise<OrdersResponse
       searchParams,
     })
     .json<ApiResponse<OrdersResponse>>();
-
-  if (!response.success || !response.data) {
-    throw new Error(response.error?.message);
-  }
-
-  return response.data;
-};
-
-export const getOrder = async (orderNumber: string): Promise<DetailOrder> => {
-  const headers = await getHeadersWithToken();
-
-  if (!headers) {
-    throw new Error('No token found');
-  }
-
-  const response = await externalApi
-    .get(`order/v1/orders/${orderNumber}`, { headers })
-    .json<ApiResponse<DetailOrder>>();
 
   if (!response.success || !response.data) {
     throw new Error(response.error?.message);
